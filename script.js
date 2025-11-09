203d5c3c56f6e381c57835ed6401db8fe541dfe2
sumeet <79735377+sumeet-darekar@users.noreply.github.com>
2025-11-09 12:43:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-//jsmon test
+//jsmon test test
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
 
 
