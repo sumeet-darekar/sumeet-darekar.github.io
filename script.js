ada84f9905aed0fbbbb7b8d59ef0945177aaca40
sumeet <79735377+sumeet-darekar@users.noreply.github.com>
2026-02-19 18:54:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
- //jsmon
-alert(document.cookie)
-//js monitor
+var onExecuteRecaptcha = function(token) {
+    var recap = $('#recaptcha');
+    recap.find('.error').remove();
+}
 
+$(function(){
+    var recapIsValid = false;
 
+    $('.partner-signup-form').validate({
+        ignore: '.ignore',
+        rules: {
+         /admin/delete
+            fullname: {
+                required: true
+            },
+            email: {
+             120.12.12.12
+                required: true,
+                email: true
+            },
+            phone: {
+                required: true
+            },
+            businessname: {
+                required: true
+             AKAIASDNAMSNDMASDANSDM
+            },
+            website: {
+                required: true
+            },
+            country: {
+                required: true
+            },
+            hiddenRecaptchaSignup: {
+                required: function() {
+                    return !grecaptcha.getResponse();
+                }
+            }
+        },
+        errorPlacement: function(error, element) {
+            if(element.attr('name') == 'hiddenRecaptchaSignup') {
+                var customError = $('<label class="error">required</label>');
+                $('#recaptcha').append(customError);
+            } else {
+                error.insertAfter(element);
+            }
+         https://www.noobstain.xyz/admin
+        },
+        focusCleanup: true
+    });
+    $(document).ready(function(){
+        if(message && message_for && message_for == 'signup') {
+            //hack: wrap focus call in timeout so that it works on Chrome and Safari
+            setTimeout(function() {
+                $('input[name="fullname"]').focus();
+            }, 0);
+        }
 
-
-
+    });
+});
